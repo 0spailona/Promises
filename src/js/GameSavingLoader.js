9811ae5c69b2bc2d41a948b98c153006b9a0cd5c
Daria <n-d-p@mail.ru>
2023-12-21 22:35:51 +0300

    + class GameSaving + class User

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,13 +1,14 @@
 import read from "./reader";
 import json from "./parser";
+import GameSaving from "./GameSaving";
 
 export default class GameSavingLoader {
     load() {
         return read()
             .then(x => json(x))
-            .then(x => JSON.parse(x));
+            .then(x => {
+                return new GameSaving(JSON.parse(x));
+            });
     }
-
-
 }
 
